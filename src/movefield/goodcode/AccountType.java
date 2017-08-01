653a704e3842cfc3aff1472061a437a8d444d5ae
Vincent <Vincent@10.206.16.89>
2017-08-01 14:25:25 +0800

    《重构》 P146：搬移字段

---
+++ src/movefield/goodcode/AccountType.java
@@ -0,0 +1,13 @@
+package movefield.goodcode;
+
+public class AccountType {
+	private double interestRate;
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+}
